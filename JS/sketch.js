a5624ea92b9a07529d6ddb38efb9f5a84dd38e1d
MatthiasKN <mkn2115@columbia.edu>
2017-05-04 18:34:39 -0400

    message

---
+++ JS/sketch.js
@@ -0,0 +1,11 @@
+
+
+// ***** Draw function ***** //
+function draw(){
+    background(255);
+    // Data Box Drawing;
+    noFill();
+    stroke(0, 0, 83);
+    strokeWeight(2);
+    rect(5, 1, 250, 140);
+ 
